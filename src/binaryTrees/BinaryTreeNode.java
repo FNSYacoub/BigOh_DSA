dd6d0ba2a0130296969a87903300bd8aad4f2ab5
fnsy <fadynabilyacoub@gmail.com>
2023-05-05 19:12:09 +0300

    Added binary tree node

---
+++ src/binaryTrees/BinaryTreeNode.java
@@ -0,0 +1,11 @@
+package binaryTrees;
+
+public class BinaryTreeNode {
+    int val;
+    BinaryTreeNode left;
+    BinaryTreeNode right;
+
+    BinaryTreeNode(int val) {
+        this.val = val;
+    }
+}
